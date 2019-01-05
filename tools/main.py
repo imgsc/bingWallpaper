10af9b83768816277307da68d3d6e516e024514a
imgsc <ryan0919@163.com>
2019-01-05 15:36:00 +0800

    change a easy method

--- tools/main.py
+++ tools/main.py
@@ -1,12 +1,31 @@
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import re,json
 
-bingUrl = "https://cn.bing.com/"
-html = requests.get(bingUrl)
-soup = BeautifulSoup(html.text,"lxml")
-#print(soup.prettify())
+def getUrl():
+	bingUrl = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1546656919100&pid=hp"
+	html = requests.get(bingUrl)
+	jsonUrl = json.loads(html.text)
+	url = jsonUrl['images'][0]['url']
+	return url
 
+def downloadImg(url):
+	jpgUrl ="https://cn.bing.com/{0}".format(url)
+	#print(jpgUrl)
+	req = requests.get(jpgUrl)
+	if req.status_code == 200:
+		with open("bingWallpaper.jpg",'wb') as f:
+			f.write(req.content)
+
+if __name__ == '__main__':
+	url = getUrl()
+	downloadImg(url)
+
+
+#print(soup.prettify())
+'''
+style = soup.select('#bgDiv')
+print(style)
 pattern  = re.compile(r"g_img=(.*?);", re.MULTILINE | re.DOTALL)
 #script = soup.find("script", text=pattern)
 jsScript = soup.body.find('script',text=pattern)
@@ -14,3 +33,4 @@ jsStr = pattern.search(jsScript.text).group(1).replace("\'","\"")
 print(jsStr)
 json = json.loads(jsStr)
 #print(json['url'])
+'''
